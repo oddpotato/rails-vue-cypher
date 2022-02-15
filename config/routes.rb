25048f734184156026502cad4f0113540406f8b9
Fiona Apps <wikipanyd@gmail.com>
2022-02-15 14:29:22 +0000

    Adding session data controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'signin/create'
+  # get 'signup/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  namespace: :api do
+  namespace :api do
     namespace :v1 do
       resources :messages
     end
